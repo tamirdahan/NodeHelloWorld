2b7cbcdf60c96579917d441fe5c1bc7888f53a71
tamirdahan <tamir.dahan@ness-tech.co.il>
2021-07-25 10:11:02 +0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -4,7 +4,7 @@ const PORT = parseInt(process.env.PORT, 10) || 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello  World</h1>');
 });
 
 server.listen(PORT, () => {
